914a0acc103eb8ee7e73d775e00a4d08d98c6bf8
wilbertnames <wiyantowilbert@gmail.com>
2021-02-15 10:45:54 -0800

    revisi 1

--- src/newfile.php
+++ src/newfile.php
@@ -3,7 +3,8 @@ $conn=new mysqli("172.18.0.3","root","inipassworddatabase","Trucorp");
 
 $query = "SELECT * FROM users";
 $result = $conn->query($query);
-$rows = mysql_num_rows($result);
+$rows = $result->num_rows;
+
 echo "Jumlah data ada ".$rows;
 
 $conn->close();
